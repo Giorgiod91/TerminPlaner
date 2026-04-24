18c463a6d48772703407861f91cb0904bf5a1da0
Giorgio Dettmar <giorgiod@profil.local>
2026-04-24 10:34:40 +0200

    added constant variables for min am max values for faster runtime and less code

--- Termin.java
+++ Termin.java
@@ -4,10 +4,13 @@ public class Termin {
 	private String title;
 	private String description;
 	private int due_date;
-	
-	
+
+	// Konstanten für min und max stufe
+	public static final int MAX_STUFE = 3;
+	public static final int MIN_STUFE = 1;
+
 	private int stufe;
-	
+
 	// Constructor
 	public Termin(String title, String description, int due_date) {
 		super();
@@ -16,17 +19,18 @@ public class Termin {
 		this.due_date = due_date;
 	}
 
+	// Setter Getter
 	public String getTitle() {
 		return title;
 	}
 
 	public void setTitle(String title) {
-		if(title !=null) {
+		if (title != null) {
 			this.title = title;
-		}else {
+		} else {
 			this.title = "Ohne Titel";
 		}
-		
+
 	}
 
 	public String getDescription() {
@@ -34,9 +38,9 @@ public class Termin {
 	}
 
 	public void setDescription(String description) {
-		if(description != null) {
-		this.description = description;
-		}else {
+		if (description != null) {
+			this.description = description;
+		} else {
 			this.description = "Keine Description";
 		}
 	}
@@ -48,43 +52,34 @@ public class Termin {
 	public void setDue_date(int due_date) {
 		this.due_date = due_date;
 	}
-	
-	
-	
+
 	// setter getter
-	
-	
-	
-	
-	
-	
-	
-	//methods
-	
+
+	// methods
+	/**
+	 * Diese Methode setzt die dringlochkeit anhand von user input
+	 * 
+	 * @param stufe
+	 * @return
+	 */
 	public String setDringlichkeit(int stufe) {
 		String neue_stufe = "";
-		if(stufe !=0) {
-			switch(stufe) {
-			case 1:
-				neue_stufe = "grün";
-				break;
-			case 2:
-				neue_stufe = "gelb";
-				break;
-			case 3:
-				neue_stufe = "rot";
-				break;
-			default:
-				break;
+		if (stufe > MIN_STUFE) {
+			switch (stufe) {
+				case 1:
+					neue_stufe = "grün";
+					break;
+				case 2:
+					neue_stufe = "gelb";
+					break;
+				case 3:
+					neue_stufe = "rot";
+					break;
+				default:
+					break;
 			}
 		}
 		return neue_stufe;
-		}
-	
-	
-	
-	
-	
-	
+	}
 
 }
